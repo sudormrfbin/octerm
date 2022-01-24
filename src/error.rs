335863745ccc919e63e77e60ef19168a669c0f9d
Gokul Soumya <gokulps15@gmail.com>
2022-01-24 21:47:06 +0530

    refactor: Separate error for github rate limiting

--- src/error.rs
+++ src/error.rs
@@ -7,9 +7,22 @@ pub enum Error {
     #[error("target html url for {api_url} not found")]
     HtmlUrlNotFound { api_url: String },
     #[error("could not communicate with github")]
-    GitHub(#[from] octocrab::Error),
+    GitHub(octocrab::Error),
+    #[error("github api rate limit exceeded")]
+    GitHubRateLimitExceeded(octocrab::Error),
     #[error("rendering error")]
     CrossTerm(#[from] crossterm::ErrorKind),
     #[error("could not complete concurrent network requests")]
     NetworkTask,
+}
+
+impl From<octocrab::Error> for Error {
+    fn from(e: octocrab::Error) -> Self {
+        if let octocrab::Error::GitHub { ref source, .. } = e {
+            if source.message.contains("rate limit exceeded") {
+                return Self::GitHubRateLimitExceeded(e);
+            }
+        }
+        return Self::GitHub(e);
+    }
 }
