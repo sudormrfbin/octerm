178017412a63763d6653fd66158aa90afd349c58
Gokul Soumya <gokulps15@gmail.com>
2022-01-26 11:56:38 +0530

    fix: Add source for Github errors

--- src/error.rs
+++ src/error.rs
@@ -2,14 +2,15 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
+    // TODO: Add InvalidToken and wrap Auth like RateLimit
     #[error("authentication with github failed")]
     Authentication,
     #[error("target html url for {api_url} not found")]
     HtmlUrlNotFound { api_url: String },
     #[error("could not communicate with github")]
-    GitHub(octocrab::Error),
+    GitHub(#[source] octocrab::Error),
     #[error("github api rate limit exceeded")]
-    GitHubRateLimitExceeded(octocrab::Error),
+    GitHubRateLimitExceeded(#[source] octocrab::Error),
     #[error("rendering error")]
     CrossTerm(#[from] crossterm::ErrorKind),
     #[error("could not complete concurrent network requests")]
