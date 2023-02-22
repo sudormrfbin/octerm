d6f89e6074a32896723e8da7022d6d73c33c8131
Gokul Soumya <gokulps15@gmail.com>
2023-02-22 15:23:54 +0530

    Add *_with_args parsecs

--- src/parser.rs
+++ src/parser.rs
@@ -3,7 +3,10 @@
 
 use crate::parsec::*;
 
-use self::types::{Adapter, Command, Consumer, Producer};
+use self::types::{
+    Adapter, AdapterWithArgs, Command, Consumer, ConsumerWithArgs, Producer, ProducerPipe,
+    ProducerWithArgs,
+};
 
 pub fn word() -> impl Fn(&str) -> ParseResult<String> {
     let parser = many1(pred(|ch| ch.is_alphanumeric()));
@@ -55,6 +58,41 @@ pub fn adapter() -> impl Fn(&str) -> ParseResult<Adapter> {
 pub fn consumer() -> impl Fn(&str) -> ParseResult<Consumer> {
     literal_to_enum(Consumer::all())
 }
+
+pub fn pipe() -> impl Fn(&str) -> ParseResult<()> {
+    map(literal("|"), |_| ())
+}
+
+pub fn producer_with_args() -> impl Fn(&str) -> ParseResult<ProducerWithArgs> {
+    let maybe_args = maybe(right(and(whitespace1(), args())));
+    map(and(producer(), maybe_args), |(producer, args)| {
+        ProducerWithArgs {
+            producer,
+            args: args.unwrap_or_default(),
+        }
+    })
+}
+
+pub fn consumer_with_args() -> impl Fn(&str) -> ParseResult<ConsumerWithArgs> {
+    let maybe_args = maybe(right(and(whitespace1(), uint_args())));
+    map(and(consumer(), maybe_args), |(consumer, args)| {
+        ConsumerWithArgs {
+            consumer,
+            args: args.unwrap_or_default(),
+        }
+    })
+}
+
+pub fn adapter_with_args() -> impl Fn(&str) -> ParseResult<AdapterWithArgs> {
+    let maybe_args = maybe(right(and(whitespace1(), args())));
+    map(and(adapter(), maybe_args), |(adapter, args)| {
+        AdapterWithArgs {
+            adapter,
+            args: args.unwrap_or_default(),
+        }
+    })
+}
+
 
 pub mod types {
     #[derive(Debug, PartialEq)]
@@ -149,6 +187,38 @@ pub mod types {
             }
         }
     }
+    // ------------------------------------------------------------------------
+    #[derive(Debug, PartialEq)]
+    pub struct ProducerWithArgs {
+        pub producer: Producer,
+        pub args: Vec<String>,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub struct ConsumerWithArgs {
+        pub consumer: Consumer,
+        pub args: Vec<usize>,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub struct AdapterWithArgs {
+        pub adapter: Adapter,
+        pub args: Vec<String>,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub struct ProducerPipe {
+        pub producer: ProducerWithArgs,
+        pub adapters: Vec<AdapterWithArgs>,
+        pub consumer: Option<ConsumerWithArgs>,
+    }
+
+    pub enum Parsed {
+        Command(Command),
+        ProducerWithArgs(ProducerWithArgs),
+        ProducerPipe(ProducerPipe),
+        ConsumerWithArgs(ConsumerWithArgs),
+    }
 }
 
 #[cfg(test)]
@@ -223,5 +293,56 @@ mod test {
         assert_eq!(parse("open"), Ok(("", Consumer::Open)));
         assert_eq!(parse("open 1 2"), Ok((" 1 2", Consumer::Open)));
         assert!(parse("list").is_err());
+    }
+
+    #[test]
+    fn test_producer_with_args() {
+        let parse = producer_with_args();
+        let test = |input, prod, args: &[&str], next_input| {
+            assert_eq!(
+                parse(input),
+                Ok((
+                    next_input,
+                    ProducerWithArgs {
+                        producer: prod,
+                        args: args.iter().map(ToString::to_string).collect(),
+                    }
+                ))
+            );
+        };
+        test("list pr open", Producer::List, &["pr", "open"], "");
+        test("list pr | done", Producer::List, &["pr"], "| done");
+        test("list pr|done", Producer::List, &["pr"], "|done");
+        test("list | done", Producer::List, &[], "| done");
+        test("list|done", Producer::List, &[], "|done");
+        test("list", Producer::List, &[], "");
+        // This is expected when using this parser; we handle this case
+        // in the producer_expr parser.
+        test("listed", Producer::List, &[], "ed");
+        // assert!(parse("listed").is_err());
+        // assert!(parse("listed pr").is_err());
+        // assert!(parse("listed | done").is_err());
+        // assert!(parse("listed|done").is_err());
+    }
+
+    #[test]
+    fn test_consumer_with_args() {
+        let parse = consumer_with_args();
+        let test = |input, cons, args: &[usize], next_input| {
+            assert_eq!(
+                parse(input),
+                Ok((
+                    next_input,
+                    ConsumerWithArgs {
+                        consumer: cons,
+                        args: args.iter().copied().collect(),
+                    }
+                ))
+            );
+        };
+        test("done 1 12", Consumer::Done, &[1, 12], "");
+        test("done", Consumer::Done, &[], "");
+        // Fake syntax
+        test("open 1 ; done", Consumer::Open, &[1], "; done");
     }
 }
